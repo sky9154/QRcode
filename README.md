faaf50d2a7a168303115dbb665b28df692c7025a
oF <sky0932039612@gmail.com>
2021-10-02 16:17:44 +0800

    完成介面以及基本串接

--- README.md
+++
@@ -1,4 +0,0 @@
-# 讓QRcode可以與GIF合併
-可將網址轉換成QRcode<br>
-並且與gif檔合併成可動的QRcode<br>
-亦或著單獨改變QRcode的背景
---
+++ gui/func.js
@@ -0,0 +1,6 @@
+eel.expose(goPython);
+build = document.getElementById("build");
+build.addeventlistener("click", goPython);
+function goPython() {
+    return "hello";
+};
